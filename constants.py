1234d06f0f2c978ba4ad147849de8be440030d73
Shaft <84402158+Shaft-3796@users.noreply.github.com>
2022-04-03 20:54:50 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,19 @@
+class Colors:
+    """
+    Terminal colors
+    """
+    LIGHT_YELLOW = '\33[91m'
+    LIGHT_GREEN = '\33[93m'
+    LIGHT_BLUE = '\33[94m'
+    LIGHT_PURPLE = '\33[95m'
+    LIGHT_CYAN = '\33[96m'
+    LIGHT_WHITE = '\33[97m'
+    RED = '\33[31m'
+    YELLOW = '\33[33m'
+    GREEN = '\33[32m'
+    BLUE = '\33[34m'
+    PURPLE = '\33[35m'
+    CYAN = '\33[36m'
+    WHITE = '\33[37m'
+    END = '\33[0m'
+    ERROR = '\x1b[0;30;41m'
